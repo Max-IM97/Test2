7a4afd9bb75d6ad432b50bc5d0a278a55afe40c6
Max-IM97 <max.matuson@gmail.com>
2017-03-22 19:41:02 -0400

    STS commit

--- src/Hi/Hello.java
+++ src/Hi/Hello.java
@@ -15,7 +15,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello my name is Max");
-		System.out.println("Yo, whats up my name is Mike");
+		System.out.println("Yo, whats up my name is Tucci");
 	}
 
 }
